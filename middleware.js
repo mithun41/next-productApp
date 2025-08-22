23688f95b80c5d29c5624484ec3cb6a78122c4f4
mithun8441 <mahmudulhasan3527@gmail.com>
2025-08-23 01:15:29 +0600

    test

--- middleware.js
+++ middleware.js
@@ -2,10 +2,12 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth({
   pages: {
-    signIn: "/dashboard",
+    signIn: "/", // login page
   },
 });
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: [
+    "/dashboard/:path*", // protect dashboard and subroutes
+  ],
 };
