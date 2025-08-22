120cd15f1fb471d507f1263ad8812905b6d8f4e1
mithun8441 <mahmudulhasan3527@gmail.com>
2025-08-22 22:25:10 +0600

    env

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -8,7 +8,7 @@ export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     CredentialsProvider({
       name: "Credentials",
@@ -42,7 +42,7 @@ export const authOptions = {
     signIn: "/login", // login page
   },
   session: { strategy: "jwt" },
-  secret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+  secret: process.env.GOOGLE_CLIENT_SECRET,
 };
 
 const handler = NextAuth(authOptions);
